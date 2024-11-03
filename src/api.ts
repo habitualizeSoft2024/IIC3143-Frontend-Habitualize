8e0b2deb24cad5a64bf64ee7edd40d34d9d51c17
blancaire <blancaire@uc.cl>
2024-11-03 14:41:19 -0300

    feat: add login and user creation endpoints

--- src/api.ts
+++ src/api.ts
@@ -1,16 +1,52 @@
 import axios, { AxiosInstance } from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.EXPO_PUBLIC_API_URL || 'https://main.rlabb.works/',
+  baseURL: process.env.EXPO_PUBLIC_API_URL || 'https://dev.rlabb.works/',
 }) as CustomAxiosInstance;
 
 interface CustomAxiosInstance extends AxiosInstance {
   getGreeting: typeof getGreeting;
+  login: typeof login;
+  createUser: typeof createUser;
 }
 
 async function getGreeting() {
   return api.get('/hello_world');
 }
 api.getGreeting = getGreeting;
+
+async function login(
+  email: string,
+  password: string,
+  username: string,
+  status: string,
+) {
+  status = status || 'active'; // Default to 'active' if not provided
+  const response = await api.post('/users/login/', {
+    email,
+    password,
+    username,
+    status,
+  });
+  return response.data; // Assuming the response contains the JWT and user ID
+}
+api.login = login;
+
+async function createUser(
+  username: string,
+  email: string,
+  password: string,
+  status: string,
+) {
+  status = status || 'active'; // Default to 'active' if not provided
+  const response = await api.post('/users', {
+    username,
+    email,
+    password,
+    status,
+  });
+  return response.data; // Assuming the response contains the created user data
+}
+api.createUser = createUser;
 
 export default api;
