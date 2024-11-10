1761be35e7132f16d86a0804664ee9566929be9d
Stephen Pugh <spugh@uc.cl>
2024-11-10 04:25:09 -0300

    feat: added login view, redirection handling and landingpage mock

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,15 @@
-import { Slot } from 'expo-router';
+import { Slot, Stack } from 'expo-router';
 import AuthProvider from '../components/contexts/AuthContext';
 
 export default function RootLayout() {
   return (
     <AuthProvider>
-      <Slot />
+      {/* <Slot /> */}
+      {/* se intercambio usar el ruteo de Slot por Stack por mejor documentacion, hablar con Benja */}
+      <Stack>
+        <Stack.Screen name="login" />
+        <Stack.Screen name="register" />
+      </Stack>
     </AuthProvider>
   );
 }
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,6 +1,8 @@
 import React, { useContext, useState } from 'react';
-import { Button, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import { AuthContext } from './contexts/AuthContext';
+import api from '@/api';
+import { useRouter } from 'expo-router';
 
 export default function LoginForm() {
   const { login, createUser } = useContext(AuthContext);
@@ -9,12 +11,22 @@ export default function LoginForm() {
   const [username, setUsername] = useState('');
   const [error, setError] = useState<string | null>(null);
 
+  const router = useRouter();
+
   const handleLogin = async () => {
     try {
-      // get response from login
-      await login(email, password, username);
+      const response = await api.login(email, password, username, status); // VERIFICAR BEN ESTO
+      if (response.token) {
+        // esto es solo una idea, buscar una forma de guardar el token en el contexto
+        // await AsyncStorage.setItem('token', response.token);
+
+        router.push('/landingpage');
+      } else {
+        console.log('Login failed. Please check your credentials.');
+      }
     } catch (error) {
-      setError('Login failed. Please check your credentials.');
+      console.error(error);
+      console.log('Login failed. Please check your credentials.');
     }
   };
 
@@ -27,32 +39,59 @@ export default function LoginForm() {
     }
   };
   return (
-    <View>
-      <TextInput
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-        autoCapitalize="none"
-        keyboardType="email-address"
-      />
-      <TextInput
-        placeholder="Username"
-        value={username}
-        onChangeText={setUsername}
-        autoCapitalize="none"
-      />
-      <TextInput
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry={true}
-      />
-      <Button title="Login" onPress={handleLogin} />
+    <div style={styles.container}>
+      <View>
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+          autoCapitalize="none"
+          keyboardType="email-address"
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Username"
+          value={username}
+          onChangeText={setUsername}
+          autoCapitalize="none"
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry={true}
+          style={styles.input}
+        />
 
-      <Text>Don't have an account?</Text>
-      <Button title="Create Account" onPress={handleCreateAccount} />
+        <Button title="Login" onPress={handleLogin} />
 
-      {error && <Text>{error}</Text>}
-    </View>
+        <Text>Don't have an account?</Text>
+
+        <Button title="Create Account" onPress={handleCreateAccount} />
+
+        {error && <Text>{error}</Text>}
+      </View>
+    </div>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 32,
+    backgroundColor: '#f4fdfc',
+    borderRadius: 6,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    backgroundColor: '#dafef6',
+    /* placeholderTextColor: 'grey', */
+    borderRadius: 6,
+    fontSize: 16,
+  },
+  button: {
+    color: '#22a098',
+  },
+});
